ad783448ff1f520c1f991bf1342d05fd74eb6740
anna-llorens <annallorens_12@hotmail.com>
2024-06-22 08:30:42 +0200

    Added routes

---
+++ src/Routes.ts
@@ -0,0 +1,12 @@
+export enum Routes {
+  // Dashboard
+  dashboard = "/dashboard",
+  // Teams
+  teams = "/teams",
+  teamOrg = "/teams/{:id}/org",
+  teamMembers = "/teams/{:id}/members",
+  teamsAssets = "/teams/assets",
+  teamsAssetDetail = "/teams/assets/detail",
+  teamsAssetDetailEdit = "/teams/assets/detail/edit",
+  teamsAssetDetailEditFlow = "/teams/assets/detail/edit/flow",
+}
